047223c6a50b7c5dc6cb53ffdc2d036d8f1c7084
Stuart Lynn <stuart.lynn@gmail.com>
2019-08-23 07:05:43 -0400

    Adding a set of functions to get word embedings from the server and to find similar words in the dataset. Not 100% giving right answers just now but basic infrastructure is in place

--- src/utils/text_clustering.js
+++ src/utils/text_clustering.js
@@ -1,74 +1,90 @@
-import * as lev  from 'fast-levenshtein';
+import * as lev from 'fast-levenshtein';
 import {KMeans} from 'ml';
-import { agnes } from 'ml-hclust';
+import {agnes} from 'ml-hclust';
 
+const cacheSimilarities = {};
 
-export const guessGroupingsLevenshteinKNN = (entries, no) => {
-  const validEntries  = entries.filter(e=> e !=="undefined") ;
-  let ans = KMeans(
-    validEntries.map((a,i)=>[i]), no, 
-    {distanceFunction: (a, b) => {
+export const suggestForMapping = (mapping, entries) => {
+  const res = [];
+  mapping.forEach(mapping_entry => {
+    entries.forEach(entry => {
+      const cacheKey = [mapping_entry, entry.name].join('_');
 
-       try{
-           let dist  = lev.get(validEntries[Math.floor(a[0])], validEntries[Math.floor(b[0])])
-           return dist       
-       }
-       catch{
-           debugger
-           return 1000 
-       }
-    }});
-
-    
-    let assignments = validEntries.reduce( (result,entry, index)=>{
-      result[entry] = ans.clusters[index]
-      return result
-    }, {})
-
-    let result= 
-    [...new Array(5)].map( (a,i)=>(
-      {
-       name: `Mapping ${i}`,
-       entries: Object.entries(assignments).filter((a)=>
-         a[1]== i 
-       ).map(a => a[0])
+      if( !mapping.includes(entry)  ){
+          if (cacheSimilarities[cacheKey]) {
+            res.push({suggestion: entry.name, score: cacheSimilarities[cacheKey]});
+          } else {
+            const dist = lev.get(mapping_entry, entry.name);
+            cacheSimilarities[cacheKey] = dist;
+            res.push({suggestion: entry.name, score: dist});
+          }
       }
-    ))
-    console.log(result)
-    return result
+    });
+  });
+  return res.sort((a, b) => (a.score > b.score ? 1 : -1)).slice(0, 5);
 };
 
-export const guessGroupingsLevenshteinHClust = (entries) => {
-  const validEntries  = entries.filter(e=> e !=="undefined") ;
-  let ans = agnes(
-    validEntries.map((a,i)=>[i]), 
-    {distanceFunction: (a, b) => {
-
-       try{
-           let dist  = lev.get(validEntries[Math.floor(a[0])], validEntries[Math.floor(b[0])])
-           return dist       
-       }
-       catch{
-           debugger
-           return 1000 
-       }
-    }});
-
-   debugger 
-    let assignments = validEntries.reduce( (result,entry, index)=>{
-      result[entry] = ans.clusters[index]
-      return result
-    }, {})
-
-    let result= 
-    [...new Array(5)].map( (a,i)=>(
-      {
-       name: `Mapping ${i}`,
-       entries: Object.entries(assignments).filter((a)=>
-         a[1]== i 
-       ).map(a => a[0])
+export const guessGroupingsLevenshteinKNN = (entries, no) => {
+  const validEntries = entries.filter(e => e !== 'undefined');
+  let ans = KMeans(validEntries.map((a, i) => [i]), no, {
+    distanceFunction: (a, b) => {
+      try {
+        let dist = lev.get(
+          validEntries[Math.floor(a[0])],
+          validEntries[Math.floor(b[0])],
+        );
+        return dist;
+      } catch {
+        debugger;
+        return 1000;
       }
-    ))
-    console.log(result)
-    return result
+    },
+  });
+
+  let assignments = validEntries.reduce((result, entry, index) => {
+    result[entry] = ans.clusters[index];
+    return result;
+  }, {});
+
+  let result = [...new Array(5)].map((a, i) => ({
+    name: `Mapping ${i}`,
+    entries: Object.entries(assignments)
+      .filter(a => a[1] == i)
+      .map(a => a[0]),
+  }));
+  console.log(result);
+  return result;
+};
+
+export const guessGroupingsLevenshteinHClust = entries => {
+  const validEntries = entries.filter(e => e !== 'undefined');
+  let ans = agnes(validEntries.map((a, i) => [i]), {
+    distanceFunction: (a, b) => {
+      try {
+        let dist = lev.get(
+          validEntries[Math.floor(a[0])],
+          validEntries[Math.floor(b[0])],
+        );
+        return dist;
+      } catch {
+        debugger;
+        return 1000;
+      }
+    },
+  });
+
+  debugger;
+  let assignments = validEntries.reduce((result, entry, index) => {
+    result[entry] = ans.clusters[index];
+    return result;
+  }, {});
+
+  let result = [...new Array(5)].map((a, i) => ({
+    name: `Mapping ${i}`,
+    entries: Object.entries(assignments)
+      .filter(a => a[1] == i)
+      .map(a => a[0]),
+  }));
+  console.log(result);
+  return result;
 };
