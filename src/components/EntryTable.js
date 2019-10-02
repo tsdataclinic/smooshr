6874209a23f4d0c54aa82d435c0fdbd7e0a5a842
Stuart Lynn <stuart.lynn@gmail.com>
2019-10-02 14:27:47 -0400

    Table of entries which is also sortable

---
+++ src/components/EntryTable.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import EntryCard from './EntryCard';
+import OrderBySelector from './OrderBySelector';
+import {faGlassCheers} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+export default function EntryTable({
+  entries,
+  page,
+  perPage,
+  style,
+  onCreateMapping,
+  onAddEntriesToMapping,
+  onToggleSelection,
+  selection,
+}) {
+  const [order, setOrder] = useState('Alphabetically');
+
+  const toggleSelection = entry => {
+    if (onToggleSelection) {
+      onToggleSelection(entry);
+    }
+  };
+
+  const orderKey = {Alphabetically: 'name', Occurances: 'count'}[order];
+  console.log('order key is ', orderKey);
+  let orderedEntries = entries.sort((a, b) =>
+    a[orderKey] > b[orderKey] ? 1 : -1,
+  );
+
+  if (orderKey === 'count') {
+    orderedEntries.reverse();
+  }
+
+  return (
+    <div style={style} className="EntryTable">
+      <div className="EntryTableFilters">
+        <OrderBySelector onChange={order => setOrder(order)} />
+      </div>
+      <div className="EntriesTableEntries">
+        {orderedEntries.length > 0 ? (
+          orderedEntries.map(entry => (
+            <EntryCard
+              key={entry.name}
+              entry={entry}
+              selected={selection.includes(entry.name)}
+              onToggleSelect={toggleSelection}
+            />
+          ))
+        ) : (
+          <div className="all-done">
+            <h1>You really smooshed that crap out of that column.</h1>
+            <h1>
+              {[...Array(5)].map(() => (
+                <FontAwesomeIcon icon={faGlassCheers} />
+              ))}
+            </h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+EntryTable.defaultProps = {
+  style: {},
+};
