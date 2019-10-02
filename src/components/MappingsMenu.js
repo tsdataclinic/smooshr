3aa2f5749db1bb268db147c81cddfdebce640243
Stuart Lynn <stuart.lynn@gmail.com>
2019-10-02 14:29:18 -0400

    Menu for mappings and a simple component to combine this with the focus view

---
+++ src/components/MappingsMenu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function MappingsMenu({
+  mappings = [],
+  selected,
+  onMappingSelected,
+  style,
+  mapping
+}) {
+
+  const mappingSelected = (mapping)=>{
+    if(onMappingSelected){
+      onMappingSelected(mapping)
+    }
+  }
+ 
+  return (
+    <div class="MappingsMenu" style={style}>
+      <h3>
+        Merged Groups <span class="Pill">{mappings.length}</span>{' '}
+      </h3>
+      <ul>
+        {mappings.map(mapping => (
+          <li
+            className={
+              selected && mapping.name == selected.name ? 'selected' : ''
+            }
+            onClick={() => mappingSelected(mapping)}>
+            {mapping.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+MappingsMenu.defaultProps = {
+  mappings: [],
+  style: {},
+};
