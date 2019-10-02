82705554c7e2e2d1aa05d1af9f9484ce9914fb46
Stuart Lynn <stuart.lynn@gmail.com>
2019-10-02 14:23:46 -0400

    Adding a proxy pass for the server to allow datasets to be downloaded from urls

--- server/api/server.py
+++ server/api/server.py
@@ -1,11 +1,13 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request,jsonify,Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres@postgres/postgres'
 db = SQLAlchemy(app)
+CHUNK_SIZE = 1024
 
 @app.route('/')
 def hello():
@@ -14,6 +16,17 @@ def hello():
 @app.route('/embedding/')
 def empty_embed():
     return jsonify([])
+
+@app.route('/proxy')
+def proxy():
+    url = request.args.get('url')
+    r = requests.get(url, stream=True, params = request.args )
+    def generate():
+      for chunk in r.iter_content(CHUNK_SIZE):
+        yield chunk
+    res = Response(generate())
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res 
 
 @app.route('/embedding/<words>')
 def embeding(words):
