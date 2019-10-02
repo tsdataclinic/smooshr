7a95a3cee6efdabca2528618ab7c4bf0c1fa8c4d
Stuart Lynn <stuart.lynn@gmail.com>
2019-10-02 14:39:01 -0400

    Changing up the welcome page

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React from 'react';
+import ProjectCard from '../components/ProjectCard';
+import {useStateValue} from '../contexts/app_context';
+import {Link} from 'react-router-dom';
 
-export default function WelcomePage(){
-  return(
-      <div className='Welcome Page'>
-          <h2>Welcome to smoosher</h2>
-          <p>Somoosher is an application that helps you clean up messy datasets</p>
+export default function WelcomePage() {
+  const [{projects}, dispatch] = useStateValue();
+  return (
+    <div className="welcome-page page">
+      <h1 className='large-title-header'>smooshr</h1>
+      <p>Wrangle those messy datasets</p>
+      {projects && (
+        <div>
+          <div className="">
+            <div className="region-header">
+              <h2>your projects</h2>
+              <Link to={'/new_project'}>
+                <button>New Project</button>
+              </Link>
+            </div>
+            <div className="region-list">
+              {projects.map(p => (
+                <Link to={`/project/${p.id}`}>
+                  <ProjectCard project={p} />
+                </Link>
+              ))}
+            </div>
+          </div>
 
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet libero enim. Curabitur convallis vel tellus quis scelerisque. Praesent tincidunt dictum elit, et blandit urna lobortis ac. Donec est orci, porttitor id egestas ultricies, tincidunt non arcu. Etiam at odio vitae nunc porta aliquam. Nunc vitae euismod elit. Aliquam sodales lobortis malesuada. Morbi mollis, tellus vel finibus mollis, nisi magna semper ex, a ornare tellus lacus at diam. Praesent non mi sed enim cursus pretium eu in leo. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer rutrum varius iaculis. Ut et arcu et nibh auctor aliquam in vel augue. Aliquam quis pulvinar sapien. Maecenas nec lacus placerat, fringilla tellus et, pulvinar sapien.</p>
-
-          <p>Praesent molestie tristique lorem tempus vulputate. Quisque egestas vel metus eu porttitor. In turpis dui, malesuada ut velit non, malesuada laoreet tellus. Aenean rutrum dapibus varius. Donec sed turpis consectetur, molestie risus vel, efficitur ex. Vestibulum ullamcorper fermentum congue. Aliquam ut elementum nibh. Vivamus placerat nisi massa, ac laoreet metus sodales vel. Sed consectetur sapien risus, vel congue felis bibendum et. Cras et viverra sapien.</p>
-
-          <p>Morbi viverra elementum ligula, pharetra commodo ante. Donec laoreet eu nisi eu ultrices. Sed auctor risus et mi lobortis feugiat. Aenean dictum pharetra commodo. Etiam sit amet sodales risus, eu bibendum orci. Phasellus lectus eros, interdum a pellentesque vel, tincidunt at arcu. Etiam turpis tellus, commodo in rutrum molestie, dictum a tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse quis diam aliquet dui suscipit dapibus. Etiam euismod ligula ut eleifend facilisis. Duis non arcu eget felis vehicula vehicula non quis odio. Mauris risus augue, tempor a odio vel, mattis vulputate sem. Ut diam arcu, volutpat ut risus eu, faucibus congue lorem. Curabitur malesuada augue sem, eu consectetur erat ultrices et. Suspendisse potenti.</p>
-
-          <p>Pellentesque non est vitae tortor dignissim facilisis quis sed lorem. Curabitur ut metus eu nisl imperdiet lacinia semper ac ligula. Duis maximus urna quis tristique sollicitudin. Vestibulum et velit fringilla, tincidunt est in, aliquet ligula. Nulla sit amet aliquet sapien. Morbi a turpis pharetra, scelerisque est et, bibendum leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. In malesuada nisl purus, sed ornare nibh imperdiet sit amet. Ut gravida tempus nisi sed rutrum. Sed aliquet aliquam nisi vel gravida. Cras accumsan massa risus, sed faucibus nunc fermentum non. Vivamus vel ultrices nibh. Nulla a odio non magna bibendum euismod. Curabitur nec neque quam. Duis at posuere sem, non viverra turpis.</p>
-
-          <p>In ullamcorper metus eget neque lacinia placerat. Donec vel semper ipsum. Praesent mollis elit nec massa porttitor posuere. Integer ut eros id tortor lacinia ultricies non vel odio. Morbi tincidunt, mi quis lacinia mollis, purus ante gravida metus, at commodo purus nibh vel elit. Donec vitae ultricies felis. Curabitur neque sem, facilisis vitae consectetur vitae, faucibus sed justo. Praesent in ultrices leo. Praesent consequat neque tempus augue mattis, vitae vehicula turpis vulputate. Fusce condimentum, enim a hendrerit posuere, augue tortor mollis libero, vel vulputate risus ex faucibus purus. Mauris at volutpat leo, non ultricies elit. Nullam in varius sem, vitae lacinia justo. Quisque eleifend eu urna tincidunt rhoncus.</p>
-      </div>
-  )
+          <div className="">
+            <div className="region-header">
+              <h2>community projects</h2>
+            </div>
+            <div className="region-list">
+              {[].map(p => (
+                <Link to={`/project/${p.id}`}>
+                  <ProjectCard project={p} />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
