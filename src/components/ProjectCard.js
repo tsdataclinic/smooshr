d49be85e414ea3e41b8f19b863be5483a605a655
Stuart Lynn <stuart.lynn@gmail.com>
2019-11-20 12:01:38 -0500

    Adding new project button, moving memory usage stats to bottom of page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,20 +2,34 @@ import React from 'react';
 import {
   faColumns,
   faDatabase,
+  faPlus,
   faAlignJustify,
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-export default function ProjectCard({ project, stats }) {
+export default function ProjectCard({project, stats}) {
   return (
     <div className="project-card card">
       <h2 className="title">{project.name}</h2>
       <p className="description">{project.description}</p>
       <div className="project-card-stats">
-        <span>{stats.columns} <FontAwesomeIcon icon={faColumns} /></span>
-        <span>{stats.datasets} <FontAwesomeIcon icon={faDatabase} /></span>
-        <span>{stats.meta_columns} <FontAwesomeIcon icon={faDatabase} /></span>
+        <span>
+          {stats.columns} <FontAwesomeIcon icon={faColumns} />
+        </span>
+        <span>
+          {stats.datasets} <FontAwesomeIcon icon={faDatabase} />
+        </span>
+        <span>
+          {stats.meta_columns} <FontAwesomeIcon icon={faDatabase} />
+        </span>
       </div>
     </div>
   );
 }
+
+export const NewProjectCard = () => (
+  <div className="project-card card new-project-card">
+    <h2 className="title">New Project</h2>
+    <FontAwesomeIcon icon={faPlus} size={'3x'} />
+  </div>
+);
