b569cf36082b693a02aa7c4cb554ebb288dc5ec4
Stuart Lynn <stuart.lynn@gmail.com>
2019-12-11 15:35:29 -0500

    Responsive menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Nav from './Nav'
 
 export default function Header({ history, match }) {
   return (
@@ -7,18 +8,13 @@ export default function Header({ history, match }) {
       <div className="logos">
         <Link style={{ display: 'flex', alignItems: 'center' }} to={'/'}>
           <img alt={"Data Clinic Modal"} src={`${process.env.PUBLIC_URL}/DataClinicLogo.png`} />{' '}
-          <span className="data-clinic">Data Clinic</span>{' '}
+          <span className="c">Data Clinic</span>{' '}
         </Link>
         <span className="project-name">smooshr (beta)</span>
       </div>
       <div className="spacer" />
-      <div className="top-menu">
-        <Link to="/">Projects</Link>
-        <Link to="/about">About</Link>
-        <a target="_blank" rel="noopener noreferrer" href="https://airtable.com/shrMCZrvP7467LNG7">
-          Feedback
-        </a>
-      </div>
+      <Nav />
+
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import {
+    faBars,
+    faTimes
+} from '@fortawesome/free-solid-svg-icons'
+
+export default function Nav() {
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = () => setShowMenu(!showMenu)
+    return (
+        <div className="top-menu">
+            <FontAwesomeIcon className='hamburger-menu' icon={faBars}
+                onClick={toggleMenu}
+            />
+            <div className={`menu-links ${showMenu ? '' : 'hidden'}`}>
+                <FontAwesomeIcon className='menu-close-icon' icon={faTimes} onClick={toggleMenu} />
+                <Link onClick={toggleMenu} to="/">Projects</Link>
+                <Link onClick={toggleMenu} to="/about">About</Link>
+                <a onClick={toggleMenu} target="_blank" rel="noopener noreferrer" href="https://airtable.com/shrMCZrvP7467LNG7">
+                    Feedback
+                </a>
+            </div>
+        </div>
+    )
+}
