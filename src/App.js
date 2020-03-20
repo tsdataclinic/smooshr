3ae4aa4db53a09a2155f2dc0818946ebc01e29ca
Stuart Lynn <stuart.lynn@gmail.com>
2020-03-20 10:30:27 -0400

    About page

--- src/App.js
+++ src/App.js
@@ -1,66 +1,69 @@
-import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer';
-import UploadModal from './components/UploadModal';
-import ProjectModal from './components/ProjectModal';
-import LoadProjectModal from './components/LoadProjectModal';
-import GHPagesRedirect from './components/GHPagesRedirect';
-import ShowApplyMappingsModal from './components/ApplyMappingsModal';
-import CodeExportModal from './components/CodeExportModal';
-import DatasetPage from './pages/DatasetPage';
-import ColumnPage from './pages/ColumnPage';
-import WelcomePage from './pages/WelcomePage';
-import ProjectPage from './pages/ProjectPage';
-import 'typeface-lato';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import UploadModal from "./components/UploadModal";
+import ProjectModal from "./components/ProjectModal";
+import LoadProjectModal from "./components/LoadProjectModal";
+import GHPagesRedirect from "./components/GHPagesRedirect";
+import ShowApplyMappingsModal from "./components/ApplyMappingsModal";
+import CodeExportModal from "./components/CodeExportModal";
+import DatasetPage from "./pages/DatasetPage";
+import ColumnPage from "./pages/ColumnPage";
+import WelcomePage from "./pages/WelcomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectPage from "./pages/ProjectPage";
+import "typeface-lato";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.scss';
+import "./App.scss";
 
 function App() {
+    return (
+        <Router basename={process.env.PUBLIC_URL}>
+            <GHPagesRedirect />
+            <div className="App">
+                <div className="main">
+                    <Route
+                        path="/project/:projectID/dataset/:datasetID"
+                        exact={true}
+                        component={DatasetPage}
+                    />
+                    <Route
+                        path="/project/:projectID/column/:columnID"
+                        exact={true}
+                        component={ColumnPage}
+                    />
+                    <Route
+                        path="/project/:projectID"
+                        exact={true}
+                        component={ProjectPage}
+                    />
+                    <Route path="/about" exact={true} component={AboutPage} />
+                    <Route path="/" exact={true} component={WelcomePage} />
+                </div>
+                <Footer />
 
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <GHPagesRedirect />
-      <div className="App">
-        <div className="main">
-          <Route
-            path="/project/:projectID/dataset/:datasetID"
-            exact={true}
-            component={DatasetPage}
-          />
-          <Route
-            path="/project/:projectID/column/:columnID"
-            exact={true}
-            component={ColumnPage}
-          />
-          <Route
-            path="/project/:projectID"
-            exact={true}
-            component={ProjectPage}
-          />
-          <Route path="/" exact={true} component={WelcomePage} />
-        </div>
-        <Footer />
+                <Route path="/" component={Header} />
 
-        <Route path="/" component={Header} />
+                <Route path="/new_project" component={ProjectModal} />
+                <Route path="/load_project" component={LoadProjectModal} />
 
-        <Route path="/new_project" component={ProjectModal} />
-        <Route path="/load_project" component={LoadProjectModal} />
+                <Route
+                    path="/project/:projectID/export"
+                    component={CodeExportModal}
+                />
+                <Route
+                    path="/project/:projectID/add_datasets"
+                    component={UploadModal}
+                />
 
-        <Route path="/project/:projectID/export" component={CodeExportModal} />
-        <Route
-          path="/project/:projectID/add_datasets"
-          component={UploadModal}
-        />
-
-        <Route
-          path="/dataset/:datasetID/apply"
-          component={ShowApplyMappingsModal}
-        />
-
-      </div>
-    </Router>
-  );
+                <Route
+                    path="/dataset/:datasetID/apply"
+                    component={ShowApplyMappingsModal}
+                />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+export default function AboutPage() {
+    return (
+        <div className="about-page">
+            <section>
+                <h2>What is smooshr?</h2>
+                <p>
+                    Introducing smooshr, a quick and friendly way to clean messy
+                    datasets.
+                </p>
+
+                <p>
+                    Datasets can be messy and as data scientists, data
+                    journalists, civic technologists or just the curious people,
+                    we spend a lot of our time trying to clean it up. That's why
+                    we built smooshr, a browser based tool that aids you in
+                    producing code that can quickly clean up unruly datasets.
+                </p>
+
+                <p>
+                    Our first release focuses on entity consolidation tasks but
+                    we have plans to extend this to other forms of data cleaning
+                    in the near future.
+                </p>
+            </section>
+            <section>
+                <h2>What is Data Clinic?</h2>
+                <p>
+                    As the data and tech philanthropic arm of the investment
+                    manager Two Sigma, Data Clinic provides pro bono data
+                    science and engineering support to nonprofits and engages in
+                    open source tooling and research that contribute to the Data
+                    for Good movement.
+                </p>
+
+                <p>
+                    We leverage Two Sigma’s people, data science skills, and
+                    technological know-how to support communities, mission
+                    driven organizations, and the broader public in their effort
+                    to use data more effectively.
+                </p>
+
+                <p>
+                    To learn more visit{" "}
+                    <a href="dataclinic.twosigma.com">
+                        dataclinic.twosigma.com
+                    </a>{" "}
+                    and connect with us via{" "}
+                    <a href="mailto:dataclinic@twosigma.com">
+                        dataclinic@twosigma.com
+                    </a>
+                    .
+                </p>
+            </section>
+
+            <section>
+                <h2>How can I get involved?</h2>
+                <p>
+                    smooshr is an open source project which means anyone can
+                    help make it better. To log bugs, suggest features or to
+                    directly help improve the code, head over to our github page
+                    at{" "}
+                    <a href="https://github.com/tsdataclinic/smooshr/">
+                        github.com/tsdataclinic/smooshr
+                    </a>
+                </p>
+
+                <p>
+                    We leverage Two Sigma’s people, data science skills, and
+                    technological know-how to support communities, mission
+                    driven organizations, and the broader public in their effort
+                    to use data more effectively.
+                </p>
+
+                <p>
+                    To learn more visit{" "}
+                    <a href="dataclinic.twosigma.com">
+                        dataclinic.twosigma.com
+                    </a>{" "}
+                    and connect with us via{" "}
+                    <a href="mailto:dataclinic@twosigma.com">
+                        dataclinic@twosigma.com
+                    </a>
+                    .
+                </p>
+            </section>
+        </div>
+    );
+}
