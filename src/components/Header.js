589ba29da662395e2fa5d83addfb6b579b70f659
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-03-22 10:46:25 -0400

    Added notice of instability

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from './Nav'
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "./Nav";
 
 export default function Header({ history, match }) {
   return (
-    <div className="header">
-      <div className="logos">
-        <Link style={{ display: 'flex', alignItems: 'center' }} to={'/'}>
-          <img alt={"Data Clinic Modal"} src={`${process.env.PUBLIC_URL}/DataClinicLogo.png`} />{' '}
-          <span className="data-clinic">Data Clinic</span>{' '}
-        </Link>
-        <span className="project-name">smooshr (beta)</span>
+    <div>
+      <div className="header">
+        <div className="logos">
+          <Link style={{ display: "flex", alignItems: "center" }} to={"/"}>
+            <img
+              alt={"Data Clinic Modal"}
+              src={`${process.env.PUBLIC_URL}/DataClinicLogo.png`}
+            />{" "}
+            <span className="data-clinic">Data Clinic</span>{" "}
+          </Link>
+          <span className="project-name">smooshr (beta)</span>
+        </div>
+        <div className="spacer" />
+        <Nav />
       </div>
-      <div className="spacer" />
-      <Nav />
-
+      <div
+        style={{
+          backgroundColor: "#ef4444",
+          color: "white",
+          padding: 12,
+          fontSize: "1.33rem",
+        }}
+      >
+        This app is currently under development and some features may not be
+        stable.
+      </div>
     </div>
   );
 }
